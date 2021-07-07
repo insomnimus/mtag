b0e7deac10c3ffc99f7ff24471d2eb7e09a4a2d2
Taylan Gökkaya <insomnimus.dev@gmail.com>
2021-07-07 22:19:54 +0300

    feat: added shell completions with build.rs

---
+++ build.rs
@@ -0,0 +1,18 @@
+use clap_generate::{
+    generate_to,
+    generators::{Bash, Elvish, Fish, PowerShell, Zsh},
+};
+include!("src/app.rs");
+
+const BIN_NAME: &str = "mtag";
+
+fn main() {
+    let mut app = new();
+    app.set_bin_name(BIN_NAME);
+    let outdir = env!("CARGO_MANIFEST_DIR");
+    generate_to::<Bash, _, _>(&mut app, BIN_NAME, outdir);
+    generate_to::<Elvish, _, _>(&mut app, BIN_NAME, outdir);
+    generate_to::<Fish, _, _>(&mut app, BIN_NAME, outdir);
+    generate_to::<PowerShell, _, _>(&mut app, BIN_NAME, outdir);
+    generate_to::<Zsh, _, _>(&mut app, BIN_NAME, outdir);
+}
